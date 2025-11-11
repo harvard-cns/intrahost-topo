8752d74f4ac493bb74e7b27518c8b6d42bc8ac50
Raj Joshi <rajkiranjoshi@gmail.com>
2025-11-11 21:30:12 +0000

    Fixed: vendor and device names not resolving using lspci

--- device_resolver.py
+++ device_resolver.py
@@ -42,10 +42,11 @@ class DeviceResolver:
                 vendor_name = None
                 device_name = None
                 for line in result.stdout.splitlines():
-                    if '\tVendor:' in line:
-                        vendor_name = line.split('\t', 1)[1].strip()
-                    elif '\tDevice:' in line:
-                        device_name = line.split('\t', 1)[1].strip()
+                    stripped = line.strip()
+                    if stripped.startswith('Vendor:'):
+                        vendor_name = stripped.split(':', 1)[1].strip()
+                    elif stripped.startswith('Device:'):
+                        device_name = stripped.split(':', 1)[1].strip()
                 if vendor_name and device_name:
                     return (vendor_name, device_name)
         except:
@@ -69,7 +70,7 @@ class DeviceResolver:
             return vendor_name
         
         # Try lspci
-        lspci_result = self._query_lspci(vendor_id, "0000")
+        lspci_result = self._query_lspci(vendor_id, "*")
         if lspci_result:
             vendor_name, _ = lspci_result
             self.vendor_cache[vendor_id] = vendor_name
